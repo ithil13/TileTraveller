abd405548c950c11cb3d95bb2bc95b075d4d9f7d
ithil13 <ithil13@gmail.com>
2018-09-19 00:30:31 +0000

    debugged and improved code

--- TileTraveller_v2.py
+++ TileTraveller_v2.py
@@ -11,66 +11,73 @@
 
 # github: https://github.com/ithil13/TileTraveller
 
+# Question 1: seinni útgáfan var auðveldari þar sem það er hægt
+# að skrifa og prófa litla hluta af kóða án þess að vera með 
+# fullklárað forrit
+#  
+# Question 2: það er mun auðveldara að lesa seinni útgáfuna, þar 
+# sem það er minna um endurtekningar og kóðanum er skipt upp 
+# í mismunandi hluta
+
+# Question 3: ég losnaði við fullt af endurteknum línum og  
+# fannst líka auðveldara að sjá leiðir til að bæta kóðann eftir
+# að ég skipti honum upp í functions
+
 def print_directions(position):
     '''prints out possible movements based on current position'''
-    if round(position, 1) == 1.1 or 2.1:
+    if round(position, 1) == 1.1 or round(position, 1) == 2.1:
         print("You can travel: (N)orth.")
-    elif round(position, 1) == 1.2:
+    if round(position, 1) == 1.2:
         print("You can travel: (N)orth or (S)outh or (E)ast.")
-    elif round(position, 1) == 1.3:
+    if round(position, 1) == 1.3:
         print("You can travel: (S)outh or (E)ast.")
-    elif round(position, 1) == 2.2 or 3.3:
+    if round(position, 1) == 2.2 or round(position, 1) == 3.3:
         print("You can travel: (S)outh or (W)est.")
-    elif round(position, 1) == 2.3:
+    if round(position, 1) == 2.3:
         print("You can travel: (W)est or (E)ast.")
-    elif round(position, 1) == 3.2:
+    if round(position, 1) == 3.2:
         print("You can travel: (N)orth or (S)outh.")
 
-def valid(position, movement):
-    '''checks if input is a valid direction'''
-    north = ["n","N"]
-    south = ["s","S"]
-    west = ["w","W"]
-    east = ["e","E"]
-    if movement in north:
-        if position == 1.1 or 1.2 or 2.1 or 3.1 or 3.2:
-            return north
-    elif movement in south:
-        if position == 1.2 or 1.3 or 2.2 or 3.2 or 3.3:
-            return south
-    elif movement in west:
-        if position == 2.2 or 2.3 or 3.3:
-            return west
-    elif movement in east:
-        if position == 1.2 or 1.3 or 2.3:
-            return east
+def is_valid(position, choice):
+    '''checks if chosen direction is valid for current position'''
+    north = ["n","N", 1.1, 1.2, 2.1, 3.1, 3.2]
+    south = ["s","S", 1.2, 1.3, 2.2, 3.2, 3.3]
+    west = ["w","W", 2.2, 2.3, 3.3]
+    east = ["e","E", 1.2, 1.3, 2.3]
+    if choice in north and round(position, 1) in north:
+        return "north"
+    elif choice in south and round(position, 1) in south:
+        return "south"
+    elif choice in west and round(position, 1) in west:
+        return "west"
+    elif choice in east and round(position, 1) in east:
+        return "east"
     else:
         return False
 
 def new_position(current, movement):
     '''calculates new position based on current location and movement input'''
-    if valid(current, movement) == "north":
+    if is_valid(current, movement) == "north":
         current += 0.1
         return current
-    elif valid(current, movement) == "south":
+    if is_valid(current, movement) == "south":
         current -= 0.1
         return current
-    elif valid(current, movement) == "west":
+    if is_valid(current, movement) == "west":
         current -= 1.0
         return current
-    elif valid(current, movement) == "east":
+    if is_valid(current, movement) == "east":
         current += 1.0
         return current
-
 
 location = 1.1
 
 while round(location, 1) != 3.1:
     print_directions(location)
     move = input("Direction: ")
-    while valid(location, move) == False:
+    while is_valid(location, move) == False:
             print("Not a valid direction!")
             move = input("Direction: ")
-    new_position(location, move)
+    location = new_position(location, move)
 else:
     print("Victory!")
